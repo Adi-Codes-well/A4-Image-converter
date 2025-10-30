a78484ea8b5cb529f8076311be856f0c144f2b17
Adi-Codes-well <jhaa27211@gmail.com>
2025-10-30 21:38:49 +0530

    Final ready product

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path"; // Import path module
 import removeBackgroundRoute from "./routes/removeBackground.js";
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use("/api/remove-background", removeBackgroundRoute);
+
+// Serve static files from the React app's build directory
+app.use(express.static(path.join(process.cwd(), "client", "dist")));
+
+// All other GET requests not handled by the API will return your React app
+app.get(/.*/, (req, res) => { // Changed "/*" to a regex to match any path
+  res.sendFile(path.resolve(process.cwd(), "client", "dist", "index.html"));
+});
 
 app.listen(5000, () => console.log("Server running on port 5000"));
